803e6722fc9c7583a2427c2413a0b8ecd4dd3b83
Piotr Rozyczko <rozyczko@gmail.com>
2018-02-16 15:50:08 +0100

    Working Model/Plugin editor SASVIEW-873

--- src/sas/qtgui/Utilities/PluginDefinition.py
+++ src/sas/qtgui/Utilities/PluginDefinition.py
@@ -20,15 +20,61 @@ from sas.qtgui.Utilities.UI.PluginDefinitionUI import Ui_PluginDefinition
 
 class PluginDefinition(QtWidgets.QDialog, Ui_PluginDefinition):
     """
+    Class describing the "simple" plugin editor.
+    This is a simple series of widgets allowing for specifying
+    model form and parameters.
     """
+    modelModified = QtCore.pyqtSignal()
     def __init__(self, parent=None):
         super(PluginDefinition, self).__init__(parent)
         self.setupUi(self)
 
         # globals
-        self.sasmodel = None
+        # model - internal representation of the new model plugin
+        #self.model = {}
+        self.initializeModel()
+        # internal representation of the parameter list
+        # {<row>: (<parameter>, <value>)}
+        self.parameter_dict = {}
 
+        # Initialize signals
         self.addSignals()
+
+        # Initialize widgets
+        self.addWidgets()
+
+    def addWidgets(self):
+        """
+        Initialize various widgets in the dialog
+        """
+        # Initial text in the function table
+        text =\
+"""y = x
+
+return y
+"""
+        self.txtFunction.insertPlainText(text)
+
+        # Validators
+        #rx = QtCore.QRegExp(r'^[\w,\s-]+$')
+        #rx = QtCore.QRegExp("[a-z-A-Z_]+")
+        rx = QtCore.QRegExp("^[A-Za-z0-9_]*$")
+
+        txt_validator = QtGui.QRegExpValidator(rx)
+        self.txtName.setValidator(txt_validator)
+
+    def initializeModel(self):
+        """
+        Define the dictionary for internal data representation
+        """
+        # Define the keys
+        self.model = {
+            'filename':'',
+            'overwrite':False,
+            'description':'',
+            'parameters':{},
+            'pd_parameters':{},
+            'text':''}
 
     def addSignals(self):
         """
@@ -47,22 +93,35 @@ class PluginDefinition(QtWidgets.QDialog, Ui_PluginDefinition):
         """
         Respond to changes in plugin name
         """
-        logging.info("onPluginNameChanged TRIGGERED")
-        pass
+        self.model['filename'] = self.txtName.text()
+        self.modelModified.emit()
 
     def onDescriptionChanged(self):
         """
         Respond to changes in plugin description
         """
-        logging.info("onDescriptionChanged TRIGGERED")
-        pass
+        self.model['description'] = self.txtDescription.text()
+        self.modelModified.emit()
 
     def onParamsChanged(self, row, column):
         """
         Respond to changes in non-polydisperse parameter table
         """
-        logging.info("onParamsChanged TRIGGERED")
-        pass
+        param = value = None
+        if self.tblParams.item(row, 0):
+            param = self.tblParams.item(row, 0).data(0)
+        if self.tblParams.item(row, 1):
+            value = self.tblParams.item(row, 1).data(0)
+
+        # If modified, just update the dict
+        self.parameter_dict[row] = (param, value)
+        self.model['parameters'] = self.parameter_dict
+
+        # Check if the update was Value for last row. If so, add a new row
+        if column == 1 and row == self.tblParams.rowCount()-1:
+            # Add a row
+            self.tblParams.insertRow(self.tblParams.rowCount())
+        self.modelModified.emit()
 
     def onParamsPDChanged(self, row, column):
         """
@@ -77,12 +136,18 @@ class PluginDefinition(QtWidgets.QDialog, Ui_PluginDefinition):
         """
         # keep in mind that this is called every time the text changes.
         # mind the performance!
-        logging.info("onFunctionChanged TRIGGERED")
-        pass
+        self.model['text'] = self.txtFunction.toPlainText().lstrip().rstrip()
+        self.modelModified.emit()
 
     def onOverwrite(self):
         """
         Respond to change in file overwrite checkbox
         """
-        logging.info("onOverwrite TRIGGERED")
-        pass
+        self.model['overwrite'] = self.chkOverwrite.isChecked()
+        self.modelModified.emit()
+
+    def getModel(self):
+        """
+        Return the current plugin model
+        """
+        return self.model
